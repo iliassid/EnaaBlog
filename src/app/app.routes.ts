39ba46899d60f3c3b374ba3ad0ada1d9fc0e82a1
omayma-gam <gamranioumayma@gmail.com>
2025-04-23 15:32:32 +0100

    Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PostDetailComponent } from './components/post-detail/post-detail.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostFormComponent } from './components/post-form/post-form.component';
 
 export const routes: Routes = [
+    {path:"home",component:HomeComponent},
+    {path:"post-detail",component:PostDetailComponent},
+    {path:"post-list",component:PostListComponent},
+    {path:"post-form",component:PostFormComponent},
+
+
+
 
 ];
