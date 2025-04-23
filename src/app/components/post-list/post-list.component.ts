bc2afcdc4461bdd3a054811889f789ea5fbaa297
omayma-gam <gamranioumayma@gmail.com>
2025-04-23 17:07:19 +0100

    component de post list

--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    DatePipe
+  ],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.scss'
 })
 export class PostListComponent {
+  projects: any;
+  totalProjects: any;
 
 }
