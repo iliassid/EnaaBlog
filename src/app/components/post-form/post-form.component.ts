90d2842b7f337f6ea8f8de18d4de365f0c4097da
Idriss-Mikdame <mikdameidriss420@gmail.com>
2025-04-24 14:51:49 +0100

    fixed Json server

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -9,14 +9,20 @@ import {PostService} from '../../services/post.service';
   styleUrl: './post-form.component.scss'
 })
 export class PostFormComponent implements OnInit{
-
+  Article : any = []
 
   constructor(private articleService : PostService) {
   }
 
   ngOnInit(): void {
-
+    this.onAllData();
   }
 
+  onAllData(){
+    this.articleService.getData().subscribe(AllData=>{
+      this.Article = AllData;
+      console.log(AllData)
+    })
+  }
 
 }
